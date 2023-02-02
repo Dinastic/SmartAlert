fc83f3bffaa38312d20f5ec69f428f229946a764
Dinastic <mmantiog@hotmail.gr>
2023-02-02 18:44:33 +0200

    Set the notify button and trying to insert values to our realtime database.

--- app/src/main/java/com/example/smartalert/NotifyActivity.java
+++ app/src/main/java/com/example/smartalert/NotifyActivity.java
@@ -14,21 +14,32 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Locale;
 
 public class NotifyActivity extends AppCompatActivity implements LocationListener, AdapterView.OnItemSelectedListener {
+    private FirebaseDatabase db;
     private Button buttonLocation,confirmDanger;
     private TextView textViewLocation;
+    private EditText  editTextComment;
     private LocationManager locationManager;
     private Spinner spinner;
     private ProgressBar progressBar;
@@ -40,6 +51,8 @@ public class NotifyActivity extends AppCompatActivity implements LocationListene
         textViewLocation = (TextView) findViewById(R.id.locationtext);
         buttonLocation = (Button) findViewById(R.id.locationButton);
         confirmDanger = (Button) findViewById(R.id.confirmDanger);
+
+        editTextComment = (EditText) findViewById(R.id.comment);
 
         spinner = (Spinner) findViewById(R.id.chooseDanger);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.dangers, android.R.layout.simple_spinner_item);
@@ -53,34 +66,50 @@ public class NotifyActivity extends AppCompatActivity implements LocationListene
                 && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},123);
         }
-        confirmDanger.setOnClickListener(new View.OnClickListener() {
+        /*confirmDanger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getLocation();
-
+                //notifyDanger();
             }
-        });
+        });*/
     }
 
-    /*@Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.confirmDanger:
-
-                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                        && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
-                }
-                    getLocation();
-                    break;
-            }
-    }*/
+    public void confirm(View view) {
+                Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
+                notifyDanger();
+    }
 
     @SuppressLint("MissingPermission")
-    private void getLocation() {
+    public void notifyDanger() {
         try {
             locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,5,this);
+            try {
+                //Gets the location
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+
+                //Initializes variables for inserting into Database.
+                String address = addresses.get(0).getAddressLine(0);
+                String comment = editTextComment.getText().toString().trim();
+                String dangerType = spinner.getSelectedItem().toString();
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                String time = timestamp.toString();
+
+                if(comment.isEmpty()){
+                    editTextComment.setError("Say something!");
+                    editTextComment.requestFocus();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+
+
+                textViewLocation.setText(address);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -89,7 +118,7 @@ public class NotifyActivity extends AppCompatActivity implements LocationListene
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
-        Toast.makeText(this, ""+location.getLatitude()+","+location.getLongitude(), Toast.LENGTH_SHORT).show();
+        /*Toast.makeText(this, ""+location.getLatitude()+","+location.getLongitude(), Toast.LENGTH_SHORT).show();
         try {
             Geocoder geocoder = new Geocoder(this, Locale.getDefault());
             List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
@@ -98,8 +127,71 @@ public class NotifyActivity extends AppCompatActivity implements LocationListene
             textViewLocation.setText(address);
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
     }
+
+    /*public void notifyDanger() {
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please provide a valid email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            editTextPassword.setError("Min password length should be 6 characters!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(fullName.isEmpty()){
+            editTextFullName.setError("Full Name is required!");
+            editTextFullName.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user = new User(fullName,email,role);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Toast.makeText(RegisterUser.this,"User has been registerd successfully!",Toast.LENGTH_LONG).show();
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                            else{
+                                                Toast.makeText(RegisterUser.this, "Failed to register. Try again!", Toast.LENGTH_SHORT).show();
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                        }
+                                    });
+                        }
+                        else{
+                            Toast.makeText(RegisterUser.this, "Failed to register.", Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }*/
 
     @Override
     public void onLocationChanged(@NonNull List<Location> locations) {
