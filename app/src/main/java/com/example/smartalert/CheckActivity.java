fb47cb97c30454d3e80c362d26d978fe201d7227
Dinastic <mmantiog@hotmail.gr>
2023-02-15 16:25:50 +0200

    General fixes

--- app/src/main/java/com/example/smartalert/CheckActivity.java
+++ app/src/main/java/com/example/smartalert/CheckActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,25 +33,15 @@ public class CheckActivity extends AppCompatActivity {
     DatabaseReference reference;
     MyAdapter myAdapter;
     ArrayList<UserCounterAlerts> list;
+    Button notifyButton , deleteButton ;
 
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check);
-
-        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
-            @Override
-            public void onComplete(@NonNull Task<String> task) {
-                if (!task.isSuccessful()){
-                    return;
-                }
-                //Get new FCM registration token
-                String token = task.getResult();
-
-            }
-        });
-
 
         recyclerView = findViewById(R.id.myList);
         reference = FirebaseDatabase.getInstance().getReference("AlertCounter");
@@ -60,8 +52,8 @@ public class CheckActivity extends AppCompatActivity {
         myAdapter = new MyAdapter(this,list);
         recyclerView.setAdapter(myAdapter);
 
-
-
+        notifyButton = (Button) findViewById(R.id.notifyButton);
+        deleteButton = (Button) findViewById(R.id.deleteButton);
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
